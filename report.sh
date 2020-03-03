64794c0e2a95dd1c28255f3a9f1bceea8f8f161f
Searinox <searinox@gmx.de>
2020-03-04 00:03:36 +0100

    c032

--- report.sh
+++ report.sh
@@ -8,64 +8,63 @@ function pause(){
 [ "$2" != "" ] && dt=$2 || read -p 'dev tag: ' dt
 
 id=$(ls -alFR /dev/disk/by-id | grep "/$disk" | awk '{print $9}' | head -n 1)
-wd=$(pwd)
+cwd=$(pwd)
+lwd="$cwd/by-dt/$dt"
+iwd="$cwd/by-id/$id"
 
-[ -d "./by-id/$id" ] || mkdir -pv "./by-dt/$dt"
+StringArray=("$lwd/$dt.id.txt" "$lwd/inxi -Dopluxxx.txt" "$lwd/df -hP.txt" "$lwd/mount -l.txt" "$lwd/lsblk -lo.txt")
 
-dir="$wd/by-id/$id"
+echo -e "\$disk\t= $disk\n\$dt \t= $dt\n\$id \t= $id\n\$cwd\t= $cwd\n\$lwd\t= $lwd\n\$iwd\t= $iwd\n" | bat
 
-[ ! -L "$dir" ] && ln -rs "$wd/by-dt/$dt" "$wd/by-id/$id"
+[ -d "$lwd/tree" ] || mkdir -pv "$lwd/tree" 2>&1 | bat
+[ ! -L "$iwd" ] && ln -rs "$lwd" "$iwd" 2>&1 | bat
 
-sudo rm "./by-id/$id/tree -a"*.txt "./by-id/$id/tree -a"*.json "./by-id/$id/tree -a"*.htm
+rm "$lwd/tree -a"*.txt "$lwd/tree -a"*.json "$lwd/tree -a"*.htm 2>&1 | bat
 
-echo $dt | tee "./by-id/$id/$dt.id.txt" | bat
-ls -alFR /dev/disk | grep "/$disk" | awk '{print $9" "$10" "$11}' | tee -a "./by-id/$id/$dt.id.txt" | bat
+log="$lwd/$dt.id.txt" ; { echo $dt ; ls -alFR /dev/disk | grep "/$disk" | awk '{print $9" "$10" "$11}' ; } | tee > "$log" ; bat --highlight-line 1 "$log"
 #pause
-#sudo hdparm -I /dev/$disk 2>&1 | tee "./by-id/$id/hdparm -I.txt" | bat
+log="$lwd/hdparm -I.txt" ; sudo hdparm -I /dev/$disk 2>&1 | tee > "$log" ; bat "$log"
 #pause
-sudo smartctl -a /dev/$disk 2>&1 | tee "./by-id/$id/smartctl -a.txt" | bat --paging never
+log="$lwd/smartctl -a.txt" ; sudo smartctl --all /dev/$disk 2>&1 | tee > "$log" ; bat --paging never "$log"
 ##pause
-sudo smartctl -x /dev/$disk 2>&1 | tee "./by-id/$id/smartctl -x.txt" | bat
+log="$lwd/smartctl -x.txt" ; sudo smartctl --xall /dev/$disk 2>&1 | tee > "$log" ; bat "$log"
 #pause
-sudo smartctl -P show /dev/$disk 2>&1 | tee "./by-id/$id/smartctl -P show.txt" | bat
+log="$lwd/smartctl -P show.txt" ; sudo smartctl --presets=show /dev/$disk 2>&1 | tee > "$log" ; bat "$log"
 #pause
-inxi -Dopluxxx | grep -A 0 "/dev/$disk " | tee "./by-id/$id/inxi -Dopluxxx.txt" | bat
-inxi -Dopluxxx | grep -A 1 "/dev/$disk[0-9]" | tee -a "./by-id/$id/inxi -Dopluxxx.txt" | bat
+log="$lwd/inxi -Dopluxxx.txt" ; { inxi -Dopluxxx | grep -A 0 "/dev/$disk " ; inxi -Dopluxxx | grep -A 1 "/dev/$disk[0-9]" ; } | tee > "$log" ; bat "$log"
 #pause
-df -hP /dev/$disk* | grep -v "udev " | tee "./by-id/$id/df -hP.txt" | bat
+log="$lwd/df -hP.txt" ; df -hP /dev/$disk* | grep -v "udev " | tee > "$log" ; bat "$log"
 #pause
-mount -l 2>&1 | grep "/dev/$disk" | tee "./by-id/$id/mount -l.txt" | bat
+log="$lwd/mount -l.txt" ; mount -l 2>&1 | grep "/dev/$disk" | tee > "$log" ; bat "$log"
 #pause
 cols="KNAME,MOUNTPOINT,UUID,FSTYPE,SIZE,MIN-IO,PHY-SEC,LOG-SEC,ROTA,TYPE,WWN,TRAN,LABEL,MODEL,SERIAL"
-lsblk -lo $cols /dev/$disk | tee "./by-id/$id/lsblk -lo.txt" | bat
+log="$lwd/lsblk -lo.txt" ; lsblk -lo $cols /dev/$disk | tee > "$log" ; bat "$log"
 #pause
-sed '1d' "./by-id/$id/lsblk -lo.txt" | while read -r line ; do
+sed '1d' "$lwd/lsblk -lo.txt" | while read -r line ; do
 	fs_kname=$(awk '{print $1}' <<< "$line")
+	echo "$line"
 	fs_mountpoint=$(lsblk -no MOUNTPOINT /dev/$fs_kname | head -n 1)
-	echo "$fs_mountpoint"
 	[ -z "$fs_mountpoint" ] && continue
 	fs_uuid=$(lsblk -no UUID /dev/$fs_kname)
-	fs_part=$(sed "s/$disk//" <<< "$fs_kname")
-#	fs_dmc=$(cut -c1-3 <<< "$fs_kname")
-	echo "$fs_part $fs_dmc"
-#	[ "$fs_dmc" == "dm-" ] && disk=$1 || read -p 'dev disk: ' disk
-#	if [ "$fs_dmc" == "dm-" ]; then
+	fs_partnr=$(sed "s/$disk//" <<< "$fs_kname")
 	if [ $(cut -c1-3 <<< "$fs_kname") == "dm-" ]; then
-		#printf 'Option -l "%s" specified\n' "$lval"
 		fs_name=$(lsblk -no NAME /dev/$fs_kname | head -n 1)
-		echo "fs_name $fs_name"
 		fs_puuid=$(sed "s/luks-//" <<< "$fs_name")
-		echo "fs_puuid $fs_puuid"
-		ls -lha /dev/disk/by-uuid
+		fs_partnr="$(blkid --uuid "$fs_puuid" | sed "s=/dev/$disk==")c"
 	fi
-#	tree -a "$fs_mountpoint" 2>&1 | tee "./by-id/$id/tree -a p$fs_part $fs_uuid.txt" | bat --paging never
-#	tree -afi "$fs_mountpoint" 2>&1 | tee "./by-id/$id/tree -afi p$fs_part $fs_uuid.txt" | bat --paging never
-#	tree -adfi "$fs_mountpoint" 2>&1 | tee "./by-id/$id/tree -adfi p$fs_part $fs_uuid.txt" | bat --paging never
-#	tree -aJ "$fs_mountpoint" 2>&1 | tee "./by-id/$id/tree -aJ p$fs_part $fs_uuid.json" | bat --paging never
-#	tree -apugsDJ --inodes --device "$fs_mountpoint" 2>&1 | tee "./by-id/$id/tree -apugsDJ --inodes --device p$fs_part $fs_uuid.json"  | bat --paging never
+	echo -e "\$fs_kname\t\t= $fs_kname\n\$fs_mountpoint\t= $fs_mountpoint\n\$fs_uuid\t\t= $fs_uuid\n\$fs_partnr\t\t= $fs_partnr\n\$fs_name\t\t= $fs_name\n\$fs_puuid\t\t= $fs_puuid\n" | bat
+	log="$lwd/tree -a p$fs_partnr $fs_uuid.txt" ; tree -a "$fs_mountpoint" 2>&1 | tee > "$log" ; bat --paging never "$log"
+	log="$lwd/tree -afi p$fs_partnr $fs_uuid.txt" ; tree -afi "$fs_mountpoint" 2>&1 | tee > "$log" ; bat --paging never "$log"
+	log="$lwd/tree -adfi p$fs_partnr $fs_uuid.txt" ; tree -adfi "$fs_mountpoint" 2>&1 | tee > "$log" ; bat --paging never "$log"
+	log="$lwd/tree -aJ p$fs_partnr $fs_uuid.json" ; tree -aJ "$fs_mountpoint" 2>&1 | tee > "$log" ; bat --paging never "$log"
+	log="$lwd/tree -apugsDJ --inodes --device p$fs_partnr $fs_uuid.json" ; tree -apugsDJ --inodes --device "$fs_mountpoint" 2>&1 | tee > "$log" ; bat --paging never "$log"
 	cd "$fs_mountpoint"
-#	tree -aH "$fs_mountpoint" 2>&1 | tee "$wd/by-id/$id/tree -aH p$fs_part $fs_uuid.htm" #| bat --paging never
-	cd "$wd"
+	log="$lwd/tree -aH p$fs_partnr $fs_uuid.htm" ; tree -aH "$fs_mountpoint" 2>&1 | tee > "$log" ; bat --paging never --wrap never "$log"
+	cd "$cwd"
+done
+
+for val in "${StringArray[@]}"; do
+  sed -i "s/$disk/sdx/g" "$val"
 done
 
 #sudo smartctl -l error /dev/sdb
